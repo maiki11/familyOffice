aa9f1345271f9a5031cd5a4472a74a2dac0653ed
EnriqueMoya <enrique.moya08@gmail.com>
2017-07-10 11:05:06 -0700

    acm1pt

---
+++ familyOffice/GalleryActions.swift
@@ -0,0 +1,22 @@
+//
+//  GalleryActions.swift
+//  familyOffice
+//
+//  Created by Enrique Moya on 06/07/17.
+//  Copyright © 2017 Leonardo Durazo. All rights reserved.
+//
+import Foundation
+import ReSwift
+
+struct InsertGalleryAction: Action {
+    let album: NSDictionary
+}
+struct DeleteGalleryAction: Action {
+    let album: Album
+}
+
+struct GetGalleyAction: Action {
+}
+struct InsertImagesAlbumAction: Action {
+    let image: ImageAlbum
+}
